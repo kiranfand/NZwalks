70e6085ff8457ab0313486317898eda891246f6b
Kiran Fand <kiranfand1912@gmail.com>
2024-04-23 22:57:27 +0530

    Master-CommitOne

---
+++ NZwalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZwalks.API.Data;
+using NZwalks.API.Models.Domain;
+
+namespace NZwalks.API.Controllers
+{
+    // https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbcontext dbContext;
+        public RegionsController(NZWalksDbcontext dbContext)
+        {
+            this.dbContext = dbContext;
+                
+        }
+        // Get ALL REGIONS
+        // GET : https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+           
+            return Ok(regions);
+        }
+        // GET SINGLE REGION or GET Region by ID
+        // GET : https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id) 
+        {
+
+            // var region = dbContext.Regions.Find(id);
+
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return Ok(region); 
+        }
+    }
+}
