fab3538c371c3cacc49a5d0fe497d01685fb6bc9
Kiran Fand <kiranfand1912@gmail.com>
2024-04-27 11:22:51 +0530

    NZ - DomainModel

---
+++ NZwalks.API/Models/DTO/RegionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace NZwalks.API.Models.DTO
+{
+    public class RegionDto
+      {
+        public Guid Id { get; set; }
+
+        public String Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? regionImageUrl { get; set; }
+
+
+    }
+}
